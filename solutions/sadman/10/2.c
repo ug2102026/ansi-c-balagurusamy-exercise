857e1a3326f641815259d2ec7891f463ac029ac4
Nayema Ferdoushi <134846919+ug2102026@users.noreply.github.com>
2023-08-03 21:26:18 +0600

    Update 2.c

--- solutions/sadman/10/2.c
+++ solutions/sadman/10/2.c
@@ -25,3 +25,30 @@ int main()
 
     return 0;
 }
+
+another way:
+#include <stdio.h>
+
+void space(int x)
+{
+    int start = 10; 
+    int end = 50;   
+
+    int sum = start;
+    for(int i=0;i<x;i++)
+    {
+        sum+=x+1;
+        printf("%d ",sum);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int numPositions = 5;
+    printf("Space of %d positions: ", numPositions);
+    space(numPositions);
+
+    return 0;
+}
